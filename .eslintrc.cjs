e5e46ca3133134311aa093775ab07d3acbd41c84
KoraReinolds <s.shurin94@gmail.com>
2024-01-02 05:53:53 +0300

    Configurete formatters
    - Added Eslint and prettier configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,48 @@
+module.exports = {
+  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended'],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'vue/no-multiple-template-root': 'off',
+    'vue/multi-word-component-names': 'off',
+    'prettier/prettier': ['error', { configFile: '.prettierrc.js' }],
+    semi: ['error', 'never'],
+  },
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        'prettier/prettier': 'off',
+        'vue/max-attributes-per-line': [
+          'error',
+          {
+            singleline: {
+              max: 1,
+            },
+            multiline: {
+              max: 1,
+            },
+          },
+        ],
+      },
+    },
+    // {
+    //   files: ['**/*.vue'],
+    //   rules: {
+    //     'vue/max-attributes-per-line': [
+    //       'error',
+    //       {
+    //         singleline: 1,
+    //         multiline: {
+    //           max: 1,
+    //           allowFirstLine: false,
+    //         },
+    //       },
+    //     ],
+    //   },
+    // },
+  ],
+}
