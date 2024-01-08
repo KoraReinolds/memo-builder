ec5f3935fee7a8da127545532a05ff27eaf596f9
KoraReinolds <s.shurin94@gmail.com>
2024-01-08 17:14:28 +0300

    'ResizedInput' Component
    - Implemented a new input component that adjusts its width based on
    content

---
+++ components/ResizedInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <span class="relative">
+    <span aria-hidden="true">
+      {{ modelValue }}
+    </span>
+    <input
+      ref="input"
+      class="absolute right-0 w-full"
+      :value="modelValue"
+      type="text"
+      @input="changeValue"
+    />
+  </span>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  const emit = defineEmits<{
+    'update:modelValue': [value: String]
+  }>()
+
+  defineProps({
+    modelValue: {
+      type: String,
+      required: true,
+    },
+  })
+
+  const input = ref<HTMLInputElement | null>(null)
+
+  const changeValue = (e: Event) => {
+    const target = e.target as HTMLInputElement
+    emit('update:modelValue', target.value)
+  }
+</script>
