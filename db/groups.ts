b67f832085fb1be640bd6613cfb0da0bd5b979d1
KoraReinolds <s.shurin94@gmail.com>
2024-02-25 00:41:34 +0300

    Refactor group-related database functions
    - Restructure functions in 'db/groups.ts' using fp style
    - Introduce 'getOrCreateGroup' for creating or retrieving groups

--- db/groups.ts
+++ db/groups.ts
@@ -1,50 +1,44 @@
 import { PrismaClient } from '@prisma/client'
+import { map, objOf, pipe } from 'ramda'
+import type { IHasID } from '~/core/id/types'
 
 const prisma = new PrismaClient()
 
-async function getGroupsByUserId(id: number) {
-  const groups = await prisma.group.findMany({
-    where: {
-      userId: id,
-    },
+interface IGroup {
+  name: string
+}
+
+interface ICreateGroupParams extends IGroup {
+  userId: number
+}
+
+export const getGroups = async (where: Partial<IGroup> & IHasID) =>
+  await prisma.group.findMany({
+    where,
     select: {
       id: true,
       name: true,
     },
   })
 
-  return groups
+export const getGroup = async (where: IGroup & IHasID) =>
+  await prisma.group.findUniqueOrThrow({ where })
+
+export const createGroup = async (data: ICreateGroupParams) =>
+  await prisma.group.create({ data })
+
+export const deleteGroups = async (data: number[]) =>
+  await pipe(
+    map((id: number) => ({ id })),
+    objOf('OR'),
+    objOf('where'),
+    prisma.group.deleteMany,
+  )(data)
+
+export const getOrCreateGroup = async (params: ICreateGroupParams & IHasID) => {
+  try {
+    return await getGroup(params)
+  } catch (e) {
+    return await createGroup(params)
+  }
 }
-
-async function getGroupByName(name: string) {
-  const group = await prisma.group.findFirst({
-    where: {
-      name,
-    },
-  })
-
-  return group
-}
-
-type CreateGroupParams = {
-  userId: number
-  name: string
-}
-
-async function createGroup(data: CreateGroupParams) {
-  const group = await prisma.group.create({ data })
-
-  return group
-}
-
-async function deleteGroups(data: number[]) {
-  const count = await prisma.group.deleteMany({
-    where: {
-      OR: data.map((id) => ({ id })),
-    },
-  })
-
-  return count
-}
-
-export { getGroupsByUserId, createGroup, getGroupByName, deleteGroups }
